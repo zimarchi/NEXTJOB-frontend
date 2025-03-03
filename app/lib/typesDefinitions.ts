1407b03765a63e994e76b4629176ae0c0d2e8d3f
Nazim MESSAOUDENE <nazimmessaoudene@Lis-MAC-23-01.local>
2025-03-03 16:57:25 +0100

    premeir commit

---
+++ app/lib/typesDefinitions.ts
@@ -0,0 +1,10 @@
+export type Candidat = {
+    id: string;
+    firstname: string;
+    lastname: string;
+    has_profile: boolean;
+    date: string;
+    // In TypeScript, this is called a string union type.
+    status: `À l'écoute du marché` | `En recherche active`;
+  };
+  
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
       <main className={styles.main}>
         <Image
           className={styles.logo}
@@ -13,38 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <span> Extrait de code avec CSS diffèrent <code>grid-row-start</code> </span>
       </main>
       <footer className={styles.footer}>
         <a
@@ -60,34 +29,6 @@ export default function Home() {
             height={16}
           />
           Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
         </a>
       </footer>
     </div>
